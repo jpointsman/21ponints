2fcfa296ee87f5ef14b5dab5e8d4e98d8aabb716
Andrei <tmp.andrei@gmail.com>
2016-07-13 14:37:43 +0300

    chane home page

--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -8,25 +8,59 @@
     stateConfig.$inject = ['$stateProvider'];
 
     function stateConfig($stateProvider) {
-        $stateProvider.state('home', {
-            parent: 'app',
-            url: '/',
-            data: {
-                authorities: []
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/home/home.html',
-                    controller: 'HomeController',
-                    controllerAs: 'vm'
+        $stateProvider
+            .state('home', {
+                parent: 'app',
+                url: '/',
+                data: {
+                    authorities: []
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/home/home.html',
+                        controller: 'HomeController',
+                        controllerAs: 'vm'
+                    }
+                },
+                resolve: {
+                    mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('home');
+                        $translatePartialLoader.addPart('points');
+                        return $translate.refresh();
+                    }]
                 }
-            },
-            resolve: {
-                mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
-                    $translatePartialLoader.addPart('home');
-                    return $translate.refresh();
+            })
+            .state('points.add', {
+                parent: 'home',
+                url: 'add/points',
+                data: {
+                    authorities: ['ROLE_USER']
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function ($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'app/entities/points/points-dialog.html',
+                        controller: 'PointsDialogController',
+                        controllerAs: 'vm',
+                        backdrop: 'static',
+                        size: 'lg',
+                        resolve: {
+                            entity: function () {
+                                return {
+                                    date: null,
+                                    exercise: null,
+                                    meals: null,
+                                    alcohol: null,
+                                    note: null,
+                                    id: null
+                                };
+                            }
+                        }
+                    }).result.then(function () {
+                        $state.go('home', null, {reload: true});
+                    }, function () {
+                        $state.go('home');
+                    });
                 }]
-            }
-        });
+            });
     }
 })();
